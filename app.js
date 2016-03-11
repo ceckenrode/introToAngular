ca1fb9ddc0514469858cd638fd68f7908615db78
ceckenrode <chriseckenrode@me.com>
2016-03-11 17:30:41 -0500

    experiementing with dependency injection

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function(){
+myApp.controller('mainController', function($scope, $log, $filter){
+
+  $scope.name = "john";
+  $scope.formattedName = $filter('uppercase')($scope.name);
+
+  $log.info($scope.name);
+  $log.info($scope.formattedName);
 
 });
