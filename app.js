5e023e0b7a5eac3ea0f7e012494cddbd1e519e2b
ceckenrode <chriseckenrode@me.com>
2016-03-11 22:12:08 -0500

    added a  to the .handle and logged out old and new values when the data changed

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController',['$scope', function($scope){
+myApp.controller('mainController', ['$scope', '$filter', '$log', function($scope, $filter, $log){
   $scope.handle = "";
+
+  $scope.lowercasehandle = function() {
+    return $filter('lowercase')($scope.handle);
+  };
+
+$scope.$watch('handle', function(newValue, oldValue) {
+console.info('Changed!');
+console.log('old: ' + oldValue);
+console.log('new: ' + newValue);
+});
+
 
 }]);
