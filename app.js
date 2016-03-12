bec037293d8ec9264a5a7726d7e586ec695ab40b
ceckenrode <chriseckenrode@me.com>
2016-03-12 00:31:40 -0500

    added  service to grab some data from the omdb api and populate the list with this data.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$filter', '$log', function($scope, $filter, $log){
+myApp.controller('mainController', ['$scope', '$filter', '$log','$http', function($scope, $filter, $log, $http) {
   $scope.handle = "";
 
   $scope.lowercasehandle = function() {
@@ -8,5 +8,23 @@ myApp.controller('mainController', ['$scope', '$filter', '$log', function($scope
   };
 
   $scope.characters = 5;
+
+  // $scope.rules = [{ rulename: "Must be at least 5 characters" }, { rulename: "Must not be used elsewhere" },
+  //   { rulename: "Must be cool" }
+
+  // ];
+
+  $http.get('http://www.omdbapi.com/?t=spider-man&y=&plot=full&r=json').success(function(result){
+    console.log(result)
+    $scope.rules = result;
+
+  }).error(function(data, status){
+    console.log(status);
+  })
+
+  $scope.alertClick = function(){
+
+    alert("clicked!");
+  }
 
 }]);
